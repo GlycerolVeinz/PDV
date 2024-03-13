bf6e266d634bb746949a921624501db732c458d0
GlycerolVeinz <safrankou.matvei@gmail.com>
2024-03-13 19:28:18 +0100

    made first version of hw02

---
+++ HomeWorks/pdv-01thrpool/src/ThreadPool.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <cstddef>
+#include <list>
+#include <vector>
+#include <thread>
+#include <condition_variable>
+
+template<typename JobT, typename WorkerT>
+class ThreadPool {
+private:
+    /** Fronta uloh. */
+    std::list<JobT> job_queue{};
+    /** Vlakna konzumentu zpracovavajicich ulohy. */
+    std::vector<std::thread> worker_threads{};
+    /** Funkce, kterou maji konzumenti vykonavat. */
+    WorkerT worker_fn;
+
+public:
+    ThreadPool(size_t thread_count, WorkerT worker) : worker_fn(worker) {
+        // Zde vytvorte "thread_count" vlaken konzumentu:
+        for (size_t i = 0; i < thread_count; i++)
+        {
+            worker_threads.push_back(std::thread([this]() {
+                worker_loop();
+            }));
+        }
+    }
+
+    /** Adds a new job to the queue.*/
+    void process(const JobT job) {
+        std::unique_lock<std::mutex> queueLock(queueMut);
+//        queueMut.lock();
+        job_queue.push_back(job);
+//        queueMut.unlock();
+        queueLock.unlock();
+        cv.notify_all();
+    }
+
+    /** Tato metoda nam umozni volajici funkci v main.cpp pockat na vsechna spustena vlakna konzumentu. */
+    void join() {
+        for (auto& worker_thread : worker_threads) {
+            worker_thread.join();
+        }
+    }
+
+private:
+    std::condition_variable cv;
+    std::mutex queueMut;
+
+    void worker_loop() {
+        while (true) {
+//            Conditional Lock
+            std::unique_lock<std::mutex> queueLock(queueMut);
+            cv.wait(queueLock, [this] { return !job_queue.empty(); });
+//          Busy waiting
+//            if (job_queue.empty()) {
+//                continue;
+//            }
+
+//            queueMut.lock();
+            auto job = job_queue.front();
+            job_queue.pop_front();
+//            queueMut.unlock();
+            queueLock.unlock();
+
+            if (!job)
+                break;
+
+            worker_fn(job);
+        }
+    }
+};
