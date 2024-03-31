ddbb0acc3cb2c958d7a35a3197da1314897d1f37
GlycerolVeinz <safrankou.matvei@gmail.com>
2024-03-31 18:09:02 +0200

    made first version of functions, starting parallelization

--- HomeWorks/pdv-04database/src/query.h
+++ HomeWorks/pdv-04database/src/query.h
@@ -18,50 +18,56 @@ bool is_satisfied_for_any(std::vector<predicate_t<row_t>> predicates, std::vecto
 
 template<typename row_t>
 bool is_satisfied_for_all(std::vector<predicate_t<row_t>> predicates, std::vector<row_t> data_table) {
-//   TODO
-    // Doimplementujte telo funkce, ktera rozhodne, zda pro VSECHNY dilci dotazy (obsazene ve
-    // vektoru 'predicates') existuje alespon jeden zaznam v tabulce (reprezentovane vektorem
-    // 'data_table'), pro ktery je dany predikat splneny.
+//   TODO parallelize this function
+    bool still_satisfies = true;
 
-    // Pro inspiraci si prostudujte kod, kterym muzete zjistit, zda prvni dilci dotaz plati,
-    // tj., zda existuje alespon jeden radek v tabulce, pro ktery predikat reprezentovany
-    // funkci predicates[i] plati:
+    for (auto predicate : predicates) {
+        for (size_t i = 0; i < data_table.size(); i++) {
+            auto row = data_table[i];
+            bool is_satisfied = predicate(row);
 
-    throw std::runtime_error{"Not implemented yet"};
+            if (is_satisfied) {
+                still_satisfies = true;
+                break;
+            } else if (!is_satisfied && i == data_table.size() - 1) {
+                still_satisfies = false;
+                break;
+            }
+        }
 
-    auto& first_predicate = predicates[0];        // Funkce reprezentujici predikat prvniho poddotazu
-
-    for (size_t i = 0; i < data_table.size(); i++) {
-        auto row = data_table[i];                // i-ty radek tabulky ...
-        bool is_satisfied = first_predicate(row);  // ... splnuje prvni predikat, pokud funkce first_predicate
-        //     vrati true
-
-        if (is_satisfied) {
-            // Nalezli jsme radek, pro ktery je predikat splneny.
-            // Dilci poddotaz tedy plati
-
-            return true;
-        } else {
-            // V opacnem pripade hledame dal - stale muzeme najit radek, pro ktery predikat plati
+        if (!still_satisfies) {
+            break;
         }
     }
 
-    // Radek, pro ktery by prvni predikat platil, jsme nenasli. Prvni dilci dotaz je tedy
-    // nepravdivy
-    return false;
+    return still_satisfies;
 }
 
 template<typename row_t>
 bool is_satisfied_for_any(std::vector<predicate_t<row_t>> predicates, std::vector<row_t> data_table) {
-//    TODO
-    // Doimplementujte telo funkce, ktera rozhodne, zda je ALESPON JEDEN dilci dotaz pravdivy.
-    // To znamena, ze mate zjistit, zda existuje alespon jeden predikat 'p' a jeden zaznam
-    // v tabulce 'r' takovy, ze p(r) vraci true.
+//    TODO parallelize this function
+    bool one_satisfies = false;
 
-    // Zamyslete se nad tim, pro ktery druh dotazu je vhodny jaky druh paralelizace. Vas
-    // kod optimalizujte na situaci, kdy si myslite, ze navratova hodnota funkce bude true.
-    // Je pro Vas dulezitejsi rychle najit splnujici radek pro jeden vybrany predikat, nebo
-    // je dulezitejsi zkouset najit takovy radek pro vice predikatu paralelne?
+    #pragma omp parallel for default(none)  shared(one_satisfies, predicates, data_table)
+    for (auto predicate : predicates) {
+        #pragma omp cancellation point for
+        for (size_t i = 0; i < data_table.size(); i++) {
+            auto row = data_table[i];
+            bool is_satisfied = predicate(row);
 
-    throw std::runtime_error{"Not implemented yet"};
+            if (is_satisfied) {
+                one_satisfies = true;
+                break;
+            } else if (!is_satisfied && i == data_table.size() - 1) {
+                one_satisfies = false;
+                break;
+            }
+        }
+
+        if (one_satisfies) {
+            #pragma omp cancel for
+        }
+    }
+
+    return one_satisfies;
 }
